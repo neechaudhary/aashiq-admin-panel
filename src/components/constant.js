212ba18fe5b9bfdf33591d75b30623d4ca0f669f
neechaudhary <neerajbhumihaar@gmail.com>
2023-05-01 16:38:49 +0530

    df

--- src/components/constant.js
+++ src/components/constant.js
@@ -1,2 +1,4 @@
-export const API= "https://jsk-backend-a4grml.dauqu.host/api"
+// export const API= "https://jsk-backend-a4grml.dauqu.host/api"
 // export const API= "http://localhost:3047/api"
+export const API= "https://aashiq-backend-production-0340.up.railway.app/api"
+
